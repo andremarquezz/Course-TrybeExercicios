6399a6233161ed2a28c3161dc7b694754e2d18f3
André Wiliams <andre.marqquez@gmail.com>
2022-02-02 15:27:06 -0300

    Adicionado Tipos Primitivos JS

---
+++ Fundamentos-Desenvolvimento-Web/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-introdução-javascript/Tipos-Primitivos-Tipagem-Dinâmica-e-Operações-Aritméticas/script.js
@@ -0,0 +1,16 @@
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId)
+
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log(area);
+const perimeter = 2 * (base + height);
+console.log(perimeter);
