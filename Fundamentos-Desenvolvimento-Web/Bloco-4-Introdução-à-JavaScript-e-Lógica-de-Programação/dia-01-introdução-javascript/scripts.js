bf7be49a931cad9ff9d15e061cdb580201d463e3
André Wiliams <andre.marqquez@gmail.com>
2022-02-02 14:27:46 -0300

    Adicionado Bloco 4

---
+++ Fundamentos-Desenvolvimento-Web/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-01-introdução-javascript/scripts.js
@@ -0,0 +1,12 @@
+const myName = "André Marquez";
+const birthCity = "São Paulo";
+let birthYear = 2000;
+console.log(myName, birthCity, birthYear)
+
+// Alteração variaveis
+let birthYear = 2030;
+console.log(birthYear)
+const birthCity = "Angra dos Reis"
+console.log(birthCity)
+
+// Recebemos uma mensagem de erro pois não é possivel alterar a constante
