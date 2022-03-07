7b6361c4dd51c99a12aca9db39a3d1f8699c5ed8
Andre Marquez <andre.marqquez@gmail.com>
2022-03-07 17:23:06 -0300

    Adiciona Dia 03

--- Fundamentos-Desenvolvimento-Web/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos-Desenvolvimento-Web/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,5 @@
 const soma = require('./sum')
 
-TestWatcher('Testa se o retorno de soma(4, 5) é 9', () => {
+test('Testa se o retorno de soma(4, 5) é 9', () => {
   expect(soma(4, 5)).toEqual(9);
 })
