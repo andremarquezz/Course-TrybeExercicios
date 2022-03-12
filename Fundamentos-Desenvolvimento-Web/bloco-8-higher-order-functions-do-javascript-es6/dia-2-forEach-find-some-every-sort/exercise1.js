e5f7ef368f04fb8b8752a42f3e4454902e09ec60
Andre Marquez <andre.marqquez@gmail.com>
2022-03-12 11:41:08 -0300

    Adiciona Exercicios

--- Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/exercise1.js
+++ Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/exercise1.js
@@ -61,7 +61,63 @@ const books = [{
 ];
 
 // Adicione o código do exercício aqui:
+// 1- Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 
-function authorBornIn1947() {
-  // escreva aqui o seu código
+// function authorBornIn1947() {
+//   const autorNascido = books.find((element) => element.author.birthYear === 1947).author.name
+//   return autorNascido
+// }
+
+// console.log(authorBornIn1947());
+
+// Retorne o nome do livro de menor nome.Dica: use a função forEach.
+
+// function smallerName() {
+//   let nameBook;
+//   books.forEach((element) => {
+//     // !namebook serve para receber o primeiro elemento na variavel - Andre Macedo
+//     if (!nameBook || element.name.length < nameBook.length) {
+//       nameBook = element
+//     }
+//   })
+//   return nameBook;
+// }
+
+// console.log(smallerName())
+
+// 3-Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+
+// function getNamedBook() {
+//   return books.find((element) => element.name.length === 26)
+// }
+
+// console.log(getNamedBook());
+
+// 4- Ordene os livros por data de lançamento em ordem decrescente.
+
+// const ordemDecrescente = books.sort((a, b) => b.releaseYear - a.releaseYear)
+
+// console.log(ordemDecrescente)
+
+// 5- Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+
+// const verificaSeculo = books.every((element) => element.author.birthYear >= 1901 && element.author.birthYear <= 2001)
+// console.log(verificaSeculo)
+
+// 6-  Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário.
+// function someBookWasReleaseOnThe80s() {
+//   const verificaDecada = books.some((element) => element.releaseYear >= 1980 && element.releaseYear <= 1989)
+//   return verificaDecada
+// }
+// console.log(someBookWasReleaseOnThe80s())
+
+
+function authorUnique() {
+  let ano;
+  books.forEach((element) => if (element.birthYear ===  ) {
+    
+  })
 }
+
+console.log(authorUnique())
--- Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/some-every.js
+++ Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-2-forEach-find-some-every-sort/some-every.js
@@ -1,10 +1,15 @@
+// 1- Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
-  arr.some(() => name)
+  const nomeAchado = arr.some((element) => element === name)
+  return nomeAchado
 }
 
 console.log(hasName(names, 'Ana'))
+
+// 2- Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
 
 const people = [{
     name: 'Mateus',
@@ -29,7 +34,8 @@ const people = [{
 ];
 
 const verifyAges = (arr, minimumAge) => {
-  arr.every()
+  const verificaIdade = arr.every((element) => element === minimumAge)
+  return verificaIdade
 }
 
 console.log(verifyAges(people, 18));
