aeeca73778869de06238e5f8af2cfc1aaaa2996f
Andre Marquez <andre.marqquez@gmail.com>
2022-03-31 19:16:23 -0300

    Commit init

---
+++ desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-react/src/Content.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends Component{
+  // Que eu fiz
+  // render() {
+  //   return <div>
+  //     <p>{conteudos.map((element) =>
+  //       `O conteudo é: ${element.conteudo}
+  //       Status: ${element.status}
+  //       Bloco: ${element.bloco}
+  //       `)}
+  //     </p>
+  //   </div>
+  // }
+  // Gabarito
+     render() {
+          return(
+            <div className="content">
+              {conteudos.map((elem) => (
+                <div key={elem.conteudo} className="card">
+                  <h4>{`O conteudo é: ${elem.conteudo}`}</h4>
+                  <p>{`status: ${elem.status}`}</p>
+                  <p>{`bloco: ${elem.bloco}`}</p>
+                </div>
+              ))}
+            </div>
+          );
+        }
+}
+
+export default Content
